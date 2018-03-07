ee0995d54431cb62b9a97f5fc263718d3fbe87d4
RED-MOKOU <37093692+RED-MOKOU@users.noreply.github.com>
2018-03-07 03:53:05 +0000

    Create 01.c

---
+++ TP1/01.c
@@ -0,0 +1,40 @@
+/*1) ¿Cuánto ocupará un arreglo de enteros de 100 elementos? ¿Y un arreglo de doubles de 100 elementos?
+Escriba y ejecute un programa que inicialice con valores aleatorios ambos arreglos, los muestre por pantalla
+y por último muestre la cantidad de bytes que ocupa cada uno. Implemente con funciones. Utilice la función
+sizeof() cuando sea necesario.*/
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define N 100
+
+void GenArr(double array[N]) {
+
+	signed int i;
+	for( i = 0; i < N; i++)
+		array[i] = rand()%10;	//asign random values from 0 to 10, can be changed accordingly.
+}
+
+void PrintArr(double array[N]) {
+
+	signed int i;
+	for( i = 0; i < N; i++)
+		printf("%2.2f ", array[i]);
+	printf("\n");
+}
+
+int main(int argc, char const *argv[])
+{
+	srand(time(NULL));
+	int a1[N];
+	double a2[N];
+	printf("Size of an int array of 100 numbers: %d bytes\n", (int)sizeof(a1));
+	printf("Size of a double array of 100 numbers: %d bytes\n", (int)sizeof(a2));
+
+	GenArr((double*)a1);
+	GenArr(a2);
+	PrintArr((double*)a1);
+	PrintArr(a2);
+	return 0;
+}
