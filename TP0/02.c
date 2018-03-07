29d835e9b2320c093e3e042bd20f2bf2a501c46a
藤原の妹紅 <37093692+RED-MOKOU@users.noreply.github.com>
2018-03-07 17:57:33 +0000

    Update 02.c

--- TP0/02.c
+++ TP0/02.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+int main(int argc, char** argv[])
 {
 	int top = -1000;
 	int bttm = 1000;
@@ -9,15 +9,15 @@ int main(int argc, char const *argv[])
 	int a = 0;
 
 	while(a != -1){
-		printf("Ingrese numero: "); scanf("%d", &a);
+		printf("Input number: "); scanf("%d", &a);
 		if(a == -1) break; //otherwise -1 is added to the total and the thing fucks up. Dunno, man, compiler is autistic.
 		top = a > top ? a : top;
 		bttm = a < bttm ? a : bttm;
 		sum += a; cnt++;
 	};
     puts("----");
-    printf("El maximo es: %d\nEl minimo es: %d\nEn total, hubo %d numeros.\n", top, bttm, cnt);
-	printf("La suma total de numeros es: %d\nEl promedio es: %.2f", sum, (float)(sum/cnt));
+    printf("Max: %d\nMin: %d\nTotal inputted numbers: %d\n", top, bttm, cnt);
+	printf("Sum: %d\nAverage: %.2f", sum, (float)(sum/cnt));
 
 	return 0;
 }
