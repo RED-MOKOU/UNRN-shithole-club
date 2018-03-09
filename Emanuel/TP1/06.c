cc10224f3cf240abff47241eabe758678446491f
藤原の妹紅 <37093692+RED-MOKOU@users.noreply.github.com>
2018-03-09 18:56:57 +0000

    Update 06.c

--- Emanuel/TP1/06.c
+++ Emanuel/TP1/06.c
@@ -4,47 +4,70 @@
 
 #define N 10
 
+void PrintArray(int*, int);
+
 /*Asigna valores aleatorios del 1 al 9 en un array[N]*/
 int* InitArray() {
 
 	int i;
-	int* ar = malloc(sizeof(int)*N);
-	int* cur = ar;
-	for( i = 0; i < N; i++) {
-		*cur = rand()%10;
-		cur++;
-	}
-	return ar;
+	int* arr = malloc(N * sizeof(int));
+	printf("sizeof(int)*N = %d\n", (int)sizeof(int)*N);
+	printf("**\nsizeof(arr) = %d\n**\n", (int)(sizeof arr));
+	for( i = 0; i < N; i++)
+		arr[i] = rand()%9+1;
+	return arr;
 }
 
-/*No sirve. Arreglar.*/
 int* ShaveAr(int array[N], int num) {
 
-	int arr2[N-1];
-	int i,j = N+1;
-	if (array[N] == num) {
+	int* arr2 = (int*)malloc(sizeof(int)*(N-1));
+	int i, j = N+1;
+	printf("Size of arr2 (top of the loop): %d\n", (int)(sizeof arr2));
+	printf("--\nArray (top of the loop):\n");
+	PrintArray(array,N);
+	puts("--");
+
+	if (array[N] == num) //if the last number in array is num (so we can use N-1 in the for loops)
+	{
 		for( i = 0; i < N-1; i++)
 			arr2[i] = array[i];
 		return arr2;
 	}
-	if (array[N] != num) {
+	else
+	{
+		printf("--\nArray (first else)");
+		PrintArray(array,N);
+		puts("--");
 		for(i = 0; i < N-1; i++) {
 			if( array[i] == num) {j = i; break;}
 			arr2[i] = array[i];
 		}
-		if(j > N) break; 
-		for (i = j; i < N-1; i++)
-			arr2[i] = array[i+1];
-		return arr2;
+		printf("j<N = %d<%d\n", j, N);
+		printf("--\nArray (j<N):\n");
+		PrintArray(array,N);
+		puts("--");
+		if(j < N) //if num is within range
+		{
+			for (i = j; i < N-1; i++)
+				arr2[i] = array[i+1];
+			puts("Return arr2");
+//			PrintArray(arr2,(N-1));
+			return arr2;
+		}
+		else
+		{
+			puts("Return array");
+			PrintArray(array,N);
+			puts("returned.");
+			return array;
+		}
 	}
-	return array;
 }
 
 void PrintArray(int* array, int size) {
 
 	int i;
-	int* a = malloc(sizeof(int)*size);
-	int* cur = a;
+	printf("print size: %d\n", size);
 	for( i = 0; i < size; i++) 
 		printf("%d ", array[i]);
 	puts("");
@@ -55,12 +78,15 @@ int main(int argc, char** argv) {
 
 	srand(time(NULL));
 	int* ar = InitArray();
+	printf("Size of ar (after init): %d (of %d)\n", (int)(sizeof ar), N);
 	int num;
 	PrintArray(ar,N);
 	puts("Ingrese un numero:");
 	scanf("%d", &num);
 	ar = ShaveAr(ar, num);
-	PrintArray
-	free(ar, (sizeof ar)/(sizeof(int)));
+	printf("Dimension of arnew: %d\n", (int)(sizeof ar));
+	PrintArray(ar, (int)(sizeof ar +1));
+	free(ar);
+//	free(arnew);
 	return 0;
 }
