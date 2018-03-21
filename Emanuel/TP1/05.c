4a667133ef1b832e4dba55e88afdf980474d5a16
藤原の妹紅 <37093692+RED-MOKOU@users.noreply.github.com>
2018-03-21 19:52:48 +0000

    Update 05.c

--- Emanuel/TP1/05.c
+++ Emanuel/TP1/05.c
@@ -1,3 +1,7 @@
+/*5) Escriba y ejecute un programa que mantenga información de números complejos (usando el tipo de datos
+definido en el trabajo práctico 0). Se desea tener 2 matrices de complejos (de 10 filas por 10 columnas) y realizar
+la suma y resta de dichas matrices. Implemente con funciones.*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -7,17 +11,16 @@ typedef struct {
 	int real, imag;
 } tsComplex[N][N];
 
-/*Usage; PrintNum(&var_add)*/
-void PrintNum(tsComplex *number) {
+void PrintNum(tsComplex number) {
 	int i, j, aux;
 	for(i = 0; i < N; i++) {
 		for(j = 0; j < N; j++) {
-			if( number[i][j]->imag < 0 ) {
-				aux = number[i][j]->imag*(-1);
-				printf("%d-%di ", number[i][j]->real, aux);
+			if( number[i][j].imag < 0 ) {
+				aux = number[i][j].imag*(-1);
+				printf("%d-%di ", number[i][j].real, aux);
 			}
 			else {
-				printf("%d+%di ", number[i][j]->real, number[i][j]->imag);
+				printf("%d+%di ", number[i][j].real, number[i][j].imag);
 			}
 		}
 	printf("\n");
@@ -25,48 +28,43 @@ void PrintNum(tsComplex *number) {
 	printf("\n");
 }
 
-/*Usage: InitNum(&var_add)*/
-void InitNum(tsComplex *num) {
+void InitNum(tsComplex num) {
 	int i, j;
 	for (i = 0; i < N; i++) {
 		for( j = 0; j < N; j++) {
-			num[i][j]->real = rand()%20-10;
-			num[i][j]->imag = rand()%20-10;
+			num[i][j].real = rand()%18-9;
+			num[i][j].imag = rand()%18-9;
 		}
 	}
 }
 
-tsComplex *Add(tsComplex *number1, tsComplex *number2) {
-	tsComplex *result;
+void Add(tsComplex result, tsComplex number1, tsComplex number2) {
 	int i, j;
 	for( i = 0; i < N; i++) {
 		for( j = 0; j < N; j++) {
-			result[i][j]->real = number1[i][j]->real + number2[i][j]->real;
-			result[i][j]->imag = number1[i][j]->imag + number2[i][j]->imag;
+			result[i][j].real = number1[i][j].real + number2[i][j].real;
+			result[i][j].imag = number1[i][j].imag + number2[i][j].imag;
 		}
 	}
-	return result;
 }
 
-tsComplex *Sub(tsComplex *number1, tsComplex *number2) {
-	tsComplex *result;
+void Sub(tsComplex result, tsComplex number1, tsComplex number2) {
 	int i, j;
 	for( i = 0; i < N; i++) {
 		for( j = 0; j < N; j++) {
-			result[i][j]->real = number1[i][j]->real - number2[i][j]->real;
-			result[i][j]->imag = number1[i][j]->imag - number2[i][j]->imag;
+			result[i][j].real = number1[i][j].real - number2[i][j].real;
+			result[i][j].imag = number1[i][j].imag - number2[i][j].imag;
 		}
 	}	
-	return result;
 }
 
 int main(int argc, char** argv) {
 
 	srand(time(NULL));
-	tsComplex num1, num2, *result;
+	tsComplex num1, num2, result;
 	char operator;
-	InitNum(&num1); InitNum(&num2);
-	PrintNum(&num1); PrintNum(&num2);
+	InitNum(num1); InitNum(num2);
+	PrintNum(num1); PrintNum(num2);
 	puts("What would you like to do? (+, -)");
 	scanf(" %c", &operator);
 	if ((operator != '+' && operator != '-' )) {
@@ -74,8 +72,8 @@ int main(int argc, char** argv) {
 		return -1;
 	}
 	switch(operator) {
-		case '+': result = Add(&num1, &num2); break;
-		case '-': result = Sub(&num1, &num2); break;
+		case '+': Add(result, num1, num2); break;
+		case '-': Sub(result, num1, num2); break;
 	}
 	PrintNum(result);
 	return 0;
