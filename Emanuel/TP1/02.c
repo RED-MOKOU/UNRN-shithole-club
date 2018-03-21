822be8cd5999af8d657a36989ffa3d666183a8c6
藤原の妹紅 <37093692+RED-MOKOU@users.noreply.github.com>
2018-03-21 18:08:24 +0000

    Update 02.c

--- Emanuel/TP1/02.c
+++ Emanuel/TP1/02.c
@@ -1,42 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-     
-int* InitArr(int row, int col) {
-     
-    signed int i, max = row*col;
-    int* arr = malloc(sizeof(int)*max);
-    int* cursor = arr;
-    for( i = 0; i < max; i++ ) {  
-        *cursor = rand()%11+20;
-        cursor++;
-    }
-    return arr;
-}
-     
-void Print(int* address, int row, int col) {
+/*2) Escriba y ejecute un programa que lea 2 enteros. Estos dos enteros definen las dimensiones de una matriz
+(filas, columnas). La matriz tiene como dimensión física NxN, y dimensión lógica filasxcolumnas (enteros leídos).
+El programa debe inicializar la matriz con valores aleatorios entre 20 y 30. Luego, se debe imprimir la
+matriz de dos formas: por filas y por columnas. Definir funciones para inicializar y mostrar la matriz. Explique:
+¿qué sucede si N es muy grande? ¿Qué sucede si N es muy pequeño?*/
 
-    int* p = address;
-    signed int i, j;
-    for( i = 0; i < row; i++ ) {
-        for( j = 0; j < col; j++ )
-            printf("%2d ", p[i*col+j]);
-        printf("\n");
-    }
+/*Si N es muy grande, se aloca memoria extra que luego el usuario no usara (por lo que se desperdician recursos).
+Por el contrario, si N es muy pequeño, la memoria alocada no alcanzara para almacenar todos los datos de la matriz.*/
+
+#include <stdio.h>
+#include <stdlib.h> //para el uso de rand()
+#include <time.h> //para la seed de rand
+#define N 100 //Se asume que la matriz no sera mayor que 100x100. Preferentemente, habria que usar memoria dinamica.
+	 
+void InitArr(int mat[N][N], int row, int col) {
+	 int i, j;
+	for( i = 0; i < row; i++ ) {  
+		for( j = 0; j < col; j++)
+			mat[i][j] = rand()%10+20;
+	}
 }
-     
+	 
+void Print(int mat[N][N], int row, int col) {
+	int i, j;
+	for( i = 0; i < row; i++ ) {
+		for( j = 0; j < col; j++ )
+			printf("%2d ", mat[i][j]);
+		printf("\n");
+	}
+}
+	 
 int main(int argc, char** argv)
 {
-    srand(time(NULL));
-    signed int a, b;
-       
-    puts("Input matrix dimensions (AxB)");
-    scanf("%d %d", &a, &b);
-    if(a < 0 || b < 0) return 1; //don't do this please.
-     
-    int* matrix = InitArr(a, b);
-    Print(matrix, a, b);
-    free(matrix);
-     
-    return 0;
+	srand(time(NULL));
+	signed int a, b, matrix[N][N];  
+	puts("Ingrese dimensiones AxB");
+	scanf("%d %d", &a, &b);
+	if(a < 0 || b < 0) {
+		printf("Error: Dimension invalida."); //la version correcta seria fprintf(stderr,"Error: Dimension invalida"), pero no tiene para este ejemplo.
+		return 1;
+	}
+	InitArr(matrix, a, b);
+	Print(matrix, a, b);
+	 
+	return 0;
 }
