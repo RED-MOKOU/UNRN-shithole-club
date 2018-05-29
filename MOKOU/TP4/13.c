e3a6fc783a8f43332734128c6726093004bb76c3
藤原の妹紅 <37093692+RED-MOKOU@users.noreply.github.com>
2018-05-29 03:16:18 +0000

    Update 13.c

--- MOKOU/TP4/13.c
+++ MOKOU/TP4/13.c
@@ -6,8 +6,8 @@ Si ambas tienen el mismo largo, y sus elementos son los mismos, entonces las col
 int CompararColas(Cola_T *cola1, Cola_T *cola2) {
 	int len1 = Longitud_Cola(*cola1);
 	int len2 = Longitud_Cola(*cola2);
-	int comparaciones;
 	if( len1 == len2 ) {
+		int comparaciones;
 		for (int i = 0; i < len1; ++i) {
 			if( Q_Pop(cola1) == Q_Pop(cola2) )
 				++comparaciones;
